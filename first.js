67dc0e95c07a25c1c3424167a4517743929a73dc
Derrick Sekidde <kidricederek@gmail.com>
2020-05-25 17:45:15 +0300

    fix error with code

--- first.js
+++ first.js
@@ -3,7 +3,6 @@ var dty = require('./mydate');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Greatest Coder Ever!');
   res.write("The date and time are currently at: " + dty.myDateTime());
-  res.end();
+  res.end('Greatest Coder Ever!');
 }).listen(8080);

1332217b0db85abfe362203956e20f6bddbcdd83
Derrick Sekidde <kidricederek@gmail.com>
2020-05-25 18:37:06 +0300

    add indention for files

--- appendfile.js
+++ appendfile.js
@@ -4,5 +4,5 @@ var fs = require('fs');
 
 fs.appendFile('mynewfile1.txt', 'Greatest Coder Ever!', function (err) {
   if (err) throw err;
-  console.log('File Saved!');
+    console.log('File Saved!');
 });

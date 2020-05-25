0d9f57890182a408d02a8f701ebc6681150c3c23
Derrick Sekidde <kidricederek@gmail.com>
2020-05-25 17:55:23 +0300

    use the url module for usage with date module

--- nyserver.js
+++ nyserver.js
@@ -7,5 +7,6 @@ http.createServer(function (req, res) {
   res.write('Remnant Greatest Coder!'); //write a response to the client
   res.write(req.url);
   var q = url.parse(req.url, true).query;
+  var txt = q.year + " " + q.month;
   res.end(); //end the response
 }).listen(8080); //the server object listens on port 8080

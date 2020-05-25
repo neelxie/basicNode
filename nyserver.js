11535fc9e18c90c8cbe3a4aa06709d666439c1a6
Derrick Sekidde <kidricederek@gmail.com>
2020-05-25 18:39:38 +0300

    add indention and comments for files

--- nyserver.js
+++ nyserver.js
@@ -3,11 +3,13 @@ var url = require('url');
 var fs = require('fs');
 
 //create a server object:
+
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write('Remnant Greatest Coder!'); //write a response to the client
   res.write(req.url);
   var q = url.parse(req.url, true).query;
   var txt = q.year + " " + q.month;
-  res.end(txt); //end the response
+  res.end(txt); 
+  //end the response
 }).listen(8080); //the server object listens on port 8080

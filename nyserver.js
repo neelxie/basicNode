bc468109f1bf610b180a0cf037ba43e0a9972eaa
Derrick Sekidde <kidricederek@gmail.com>
2020-05-25 17:56:10 +0300

    use the url module and pick response

--- nyserver.js
+++ nyserver.js
@@ -8,5 +8,5 @@ http.createServer(function (req, res) {
   res.write(req.url);
   var q = url.parse(req.url, true).query;
   var txt = q.year + " " + q.month;
-  res.end(); //end the response
+  res.end(txt); //end the response
 }).listen(8080); //the server object listens on port 8080

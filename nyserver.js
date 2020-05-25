59f8c1f3a3307af1beea7b32da310ba7bde4d6c5
Derrick Sekidde <kidricederek@gmail.com>
2020-05-25 17:51:51 +0300

    create server response url

--- nyserver.js
+++ nyserver.js
@@ -4,5 +4,6 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write('Remnant Greatest Coder!'); //write a response to the client
+  res.write(req.url);
   res.end(); //end the response
 }).listen(8080); //the server object listens on port 8080

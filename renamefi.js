1be8f0bd03476f19cc9e4c938ee7d5ae50721892
Derrick Sekidde <kidricederek@gmail.com>
2020-05-26 17:04:13 +0300

    add rename file contents

--- renamefi.js
+++ renamefi.js
@@ -3,5 +3,5 @@ var fs = require('fs');
 
 fs.rename('mynewfile1.txt', 'myrenamedfile.txt', function (err) {
   if (err) throw err;
-  console.log('File Renamed!');
+  console.log('This File Renamed!');
 });

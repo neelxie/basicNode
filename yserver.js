39a49de62fa258816d1f885f0107a09ce7720fa9
Derrick Sekidde <kidricederek@gmail.com>
2020-05-26 18:14:57 +0300

    add new server file content indent

--- yserver.js
+++ yserver.js
@@ -9,7 +9,7 @@ http.createServer(function (req, res) {
   fs.readFile(filename, function(err, data) {
     if (err) {
       res.writeHead(404, {'Content-Type': 'text/html'});
-      return res.end("404 Not Found");
+      return res.end("Page 404 Not Found");
     } 
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);

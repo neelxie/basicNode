613597a918c618859352865f901b6757c687c2e7
Derrick Sekidde <kidricederek@gmail.com>
2020-05-27 17:52:37 +0300

    Add event

--- yevent.js
+++ yevent.js
@@ -2,5 +2,5 @@
 var fs = require('fs');
 var rs = fs.createReadStream('./demofile.txt');
 rs.on('open', function () {
-  console.log('The file is open');
+  console.log('The file is open now');
 });

e76d8f9b42fae83825182c5435f3b023a395241c
Derrick Sekidde <kidricederek@gmail.com>
2020-05-27 19:03:46 +0300

    Add event handler function

--- yserver.js
+++ yserver.js
@@ -9,9 +9,11 @@ http.createServer(function (req, res) {
   var q = url.parse(req.url, true);
   var filename = "." + q.pathname;
   var eventEmitter = new events.EventEmitter();
-  
-  //Create an event handler:
 
+  //Create an event handler:
+  var myEventHandler = function () {
+    console.log('I hear a scream!');
+  }
 
   fs.readFile(filename, function(err, data) {
     if (err) {

cca42cf3f24770a20586c35c93043b67186a50f4
Derrick Sekidde <kidricederek@gmail.com>
2020-05-27 17:53:55 +0300

    Add event import

--- yserver.js
+++ yserver.js
@@ -2,6 +2,8 @@
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
+var events = require('events');
+
 
 http.createServer(function (req, res) {
   var q = url.parse(req.url, true);

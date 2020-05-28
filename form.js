f599f042359c47227e4a6e9edf65773e8ef0d1c4
Derrick Sekidde <kidricederek@gmail.com>
2020-05-28 16:21:13 +0300

    Add writehead add form submit

--- form.js
+++ form.js
@@ -4,6 +4,7 @@ http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
     
+    res.write('<input type="submit">');
     res.write('</form>');
     return res.end();
   }).listen(8080);

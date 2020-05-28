0f0da337efb9c2204e09b1f0269e2302d30813db
Derrick Sekidde <kidricederek@gmail.com>
2020-05-28 16:58:26 +0300

    add formidable if else indention

--- form.js
+++ form.js
@@ -6,11 +6,11 @@ http.createServer(function (req, res) {
   if (req.url == '/fileupload') {
   }
   else {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-  res.write('<input type="file" name="filetoupload"><br>');
-  res.write('<input type="submit">');
-  res.write('</form>');
-  return res.end();
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+    res.write('<input type="file" name="filetoupload"><br>');
+    res.write('<input type="submit">');
+    res.write('</form>');
+    return res.end();
   }
 }).listen(8080);

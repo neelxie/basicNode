94852cb5fe0f6d7953e3ad5b22a53be18d668612
Derrick Sekidde <kidricederek@gmail.com>
2020-05-28 16:21:38 +0300

    Add writehead add file upload

--- form.js
+++ form.js
@@ -3,7 +3,7 @@ var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-    
+    res.write('<input type="file" name="filetoupload"><br>');
     res.write('<input type="submit">');
     res.write('</form>');
     return res.end();

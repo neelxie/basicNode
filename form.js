fb997377a96ae428204bd00896bd495cc090f67f
Derrick Sekidde <kidricederek@gmail.com>
2020-05-30 21:13:01 +0300

    file upload path if

--- form.js
+++ form.js
@@ -9,6 +9,7 @@ http.createServer(function (req, res) {
     form.parse(req, function (err, fields, files) {
         var oldpath = files.filetoupload.path;
         var newpath = 'C:/Users/Greatest/' + files.filetoupload.name;
+        fs.rename(oldpath, newpath, function (err) {});
         res.write('File uploaded');
         res.end();
     });

47b2cd6abacef94a3f8454df4e4168a5ad42096d
Derrick Sekidde <kidricederek@gmail.com>
2020-05-31 04:55:41 +0300

    linting

--- mailerfile.js
+++ mailerfile.js
@@ -8,7 +8,7 @@ var mailOptions = {};
 transporter.sendMail(mailOptions, function(error, info){
     if (error) {
         console.log(error);
-      } else {
+    } else {
         console.log('Email sent: ' + info.response);
-      }    
+    }    
 });

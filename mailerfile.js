364b4956445b27d249e038e9a0380b6dc250661a
Derrick Sekidde <kidricederek@gmail.com>
2020-05-31 05:02:44 +0300

    add html

--- mailerfile.js
+++ mailerfile.js
@@ -13,7 +13,8 @@ var mailOptions = {
     from: 'youremail@gmail.com',
     to: 'myfriend@yahoo.com', 'otherperson@yahoo.com',
     subject: 'Sending you Email using Node.js',
-    text: 'That was easy!'  
+    //text: 'That was easy!' 
+    html: '<h1>Welcome</h1><p>That was super easy!</p>'
 };
 
 transporter.sendMail(mailOptions, function(error, info){

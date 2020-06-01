cb33a0e52e56af62acb3dca15a8d882f71866aa3
Derrick Sekidde <kidricederek@gmail.com>
2020-06-01 09:32:57 +0300

    Add changes

--- mailerfile.js
+++ mailerfile.js
@@ -21,6 +21,6 @@ transporter.sendMail(mailOptions, function(error, info){
     if (error) {
         console.log(error);
     } else {
-        console.log('Email sent: ' + info.response);
+        console.log('Email has been sent: ' + info.response);
     }    
 });
